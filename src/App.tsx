627267ab943ad73a6a52c8cc8a284211551b3617
Aurelien Chaillot <chaillot.aurelien@gmail.com>
2021-04-28 11:29:23 +0200

    Apollo client setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
 import React, { FC } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    split,
+    HttpLink,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import RandomChat from './components/pages/RandomChatPage/RandomChat';
 import HomePage from './components/pages/HomePage/HomePage';
+import './App.css';
+
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:9000/subscriptions',
+    options: {
+        reconnect: true,
+    },
+});
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:9000/graphql',
+    credentials: 'include',
+});
+
+const link = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink
+);
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+});
 
 const App: FC = () => {
     return (
-        <Router>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/random-chat" component={RandomChat} />
-        </Router>
+        <ApolloProvider client={client}>
+            <Router>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/random-chat" component={RandomChat} />
+            </Router>
+        </ApolloProvider>
     );
 };
 
